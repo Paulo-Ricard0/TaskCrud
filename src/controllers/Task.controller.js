d8c56b8a5ac9cd982d96d6a39279eaf3f4bdb114
Paulo Ricardo <pr741234@gmail.com>
2024-01-31 22:54:12 -0300

    feat: Implementado controller de task

---
+++ src/controllers/Task.controller.js
@@ -0,0 +1,45 @@
+import { NotFoundError } from "../helpers/api-errors.js";
+import { Task } from "../models/Task.js";
+
+class TaskController {
+  async create(req, res) {
+    const task = await Task.create(req.body);
+    return res.status(201).json(task);
+  }
+
+  async findAll(req, res) {
+    let { limit } = req.query;
+    const TaskList = await Task.findAll({ limit });
+    return res.json(TaskList);
+  }
+
+  async findOne(req, res) {
+    const task = await Task.findByPk(req.params.id);
+    if (!task) {
+      throw new NotFoundError("Tarefa não encontrada.");
+    }
+    return res.json(task);
+  }
+
+  async updateOne(req, res) {
+    const filter = { where: { id: req.params.id } };
+    const task = await Task.update(req.body, filter);
+
+    if (task[0] > 0) {
+      return res.json({ message: "Tarefa atualizada com sucesso!" });
+    }
+    throw new NotFoundError("Tarefa não encontrada.");
+  }
+
+  async deleteOne(req, res) {
+    const filter = { where: { id: req.params.id } };
+    const task = await Task.destroy(filter);
+
+    if (task > 0) {
+      return res.json({ message: "Tarefa deletada com sucesso!" });
+    }
+    throw new NotFoundError("Tarefa não encontrada.");
+  }
+}
+
+export default new TaskController();
