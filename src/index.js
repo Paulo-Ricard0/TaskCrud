1e995d16857a8ac631b3c8ccab2572f43fbefd44
Paulo Ricardo <pr741234@gmail.com>
2024-01-31 22:49:39 -0300

    feat: Configurado servidor e uso das rotas

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import "express-async-errors";
+import express from "express";
+import dotenv from "dotenv";
+import { sequelize } from "./database.js";
+import "./models/Task.js";
+import "./models/User.js";
+import taskRoutes from "./routes/Task.routes.js";
+import userRoutes from "./routes/user.routes.js";
+import { logRoutes } from "./middlewares/log-routes.middleware.js";
+import { erroMiddleware } from "./middlewares/error.middleware.js";
+
+dotenv.config();
+sequelize
+  .authenticate()
+  .then(async () => {
+    await sequelize.sync();
+    console.log("Conexão estabelecida com sucesso!.");
+    const app = express();
+
+    app.use(express.json());
+    app.use(logRoutes);
+    app.use("/task", taskRoutes);
+    app.use("/user", userRoutes);
+    app.use(erroMiddleware);
+
+    const port = process.env.PORT;
+    return app.listen(port, () =>
+      console.log(`Servidor iniciado na porta: http://localhost:${port}`)
+    );
+  })
+  .catch((error) =>
+    console.error("Erro ao conectar ao banco de dados:", error)
+  );
