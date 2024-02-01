52578c0eabc748db87c0e8beb46b6cfa70a11dc9
Paulo Ricardo <pr741234@gmail.com>
2024-01-31 22:48:11 -0300

    feat: Implementado rotas de user

---
+++ src/routes/user.routes.js
@@ -0,0 +1,10 @@
+import express from "express";
+const router = express.Router();
+import controller from "../controllers/User.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+
+router.route("/register").post(controller.create);
+router.route("/login").post(controller.login);
+router.route("/profile").get(authMiddleware, controller.getProfile);
+
+export default router;
