5c8e88256fafdb9f216f04601e03805d3aa31138
Paulo Ricardo <pr741234@gmail.com>
2024-04-03 17:26:42 -0300

    refactor: Atualiza conexões do servidor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "express-async-errors";
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { sequelize } from "./database.js";
+import { testConnection } from "./database.js";
 import "./models/Task.js";
 import "./models/User.js";
 import taskRoutes from "./routes/Task.routes.js";
@@ -11,25 +11,18 @@ import { logRoutes } from "./middlewares/log-routes.middleware.js";
 import { erroMiddleware } from "./middlewares/error.middleware.js";
 
 dotenv.config();
-sequelize
-  .authenticate()
-  .then(async () => {
-    await sequelize.sync();
-    console.log("Conexão estabelecida com sucesso!.");
-    const app = express();
 
-    app.use(cors({ origin: "*" }));
-    app.use(express.json());
-    app.use(logRoutes);
-    app.use("/task", taskRoutes);
-    app.use("/user", userRoutes);
-    app.use(erroMiddleware);
+const app = express();
 
-    const port = process.env.PORT;
-    return app.listen(port, () =>
-      console.log(`Servidor iniciado na porta: http://localhost:${port}`)
-    );
-  })
-  .catch((error) =>
-    console.error("Erro ao conectar ao banco de dados:", error)
-  );
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(logRoutes);
+app.use("/task", taskRoutes);
+app.use("/user", userRoutes);
+app.use(erroMiddleware);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  testConnection();
+  console.log(`Servidor rodando na porta ${port}`);
+});
