cd5cd31c39b3d1588d8b9e170236aabd2f8b6032
Paulo Ricardo <pr741234@gmail.com>
2024-04-03 17:07:33 -0300

    feat: Atualiza rotas

--- src/routes/Task.routes.js
+++ src/routes/Task.routes.js
@@ -3,13 +3,17 @@ const router = express.Router();
 import controller from "../controllers/Task.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 
-router.use(authMiddleware);
-router.route("/").get(controller.findAll).post(controller.create);
+// router.use(authMiddleware);
+router
+  .route("/")
+  .get(controller.findAll)
+  .post(authMiddleware, controller.create);
 
 router
   .route("/:id")
   .get(controller.findOne)
-  .put(controller.updateOne)
-  .delete(controller.deleteOne);
+  .put(authMiddleware, controller.updateOne)
+  .put(authMiddleware, controller.updateOne)
+  .delete(authMiddleware, controller.deleteOne);
 
 export default router;
